3e395b0dea08dbd062ed988f1b14d50fd6f88e5f
Andrew Phoenix <mrtopnacho@gmail.com>
2023-11-25 20:46:10 -0500

    Represents a single cell in the game of life

---
+++ src/cell.rs
@@ -0,0 +1,53 @@
+#[derive(Clone, Copy)]
+pub enum Cell {
+    Alive,
+    Dead,
+}
+
+impl std::default::Default for Cell {
+    fn default() -> Self {
+        Cell::Dead
+    }
+}
+impl std::fmt::Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Cell::Alive => "O",
+            Cell::Dead => "X",
+        })
+    }
+}
+impl Cell {
+    pub fn make_dead(&mut self) {
+       *self = Cell::Dead;
+    }
+
+    pub fn make_alive(&mut self) {
+        *self = Cell::Alive;
+    }
+
+    pub fn change_state(&mut self) {
+        match self {
+            Cell::Dead => self.make_alive(),
+            Cell::Alive => self.make_dead(),
+        };
+    }
+    
+    pub fn update(&mut self, neighbor_count: usize) {
+        match self {
+            Cell::Alive => {
+                match neighbor_count {
+                    0 | 1 => self.make_dead(),
+                    2 | 3 => (),
+                    _ => self.make_dead()
+                }   
+            },
+            Cell::Dead => {
+                match neighbor_count {
+                    3 => self.make_alive(),
+                    _ => (),
+                }
+            },
+        };
+    }
+}
