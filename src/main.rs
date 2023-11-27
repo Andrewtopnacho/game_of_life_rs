bcaa1788e0c9cf84b962b9fb5049cdc601c60cd4
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-27 01:29:09 -0500

    renamed var

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,24 @@ use macroquad::prelude::*;
 
 #[macroquad::main("life")]
 async fn main() {
-    let mut primary_board = Board::<100, 100>::random();
+    let mut board = Board::<100, 100>::random();
     let mut update_toggle = false;
     
     loop {
         if is_key_pressed(KeyCode::F1) {
-            primary_board = Board::random();
+            board = Board::random();
         }
         else if is_key_pressed(KeyCode::F4) {
-            primary_board = Board::glider_gun()
+            board = Board::glider_gun()
         }
         if is_key_pressed(KeyCode::F3) {
            update_toggle = !update_toggle;
         }
         if is_key_pressed(KeyCode::F2) || update_toggle {
-            primary_board.update();
+            board.update();
         }
 
-        primary_board.macroquad_draw();
+        board.macroquad_draw();
 
         next_frame().await;
     }
