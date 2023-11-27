d9197052ed637d937527bb5d89671531f2fa305f
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-27 00:44:47 -0500

    reformated input checks

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,20 @@ use macroquad::prelude::*;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut primary_board = Board::random();
     let mut update_toggle = false;
+    
     loop {
         clear_background(BLACK);
 
         if is_key_pressed(KeyCode::F1) {
             primary_board = Board::random();
         }
-
-        if is_key_pressed(KeyCode::F3) {
-           update_toggle = !update_toggle;
-        }
-
         if is_key_pressed(KeyCode::F2) || update_toggle {
             primary_board.update();
         }
-
+        if is_key_pressed(KeyCode::F3) {
+           update_toggle = !update_toggle;
+        }
+        
         primary_board.macroquad_draw();
 
         next_frame().await;
