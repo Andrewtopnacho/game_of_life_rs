ecd9b1c11fa641f4174bb0b66d80cb46d91a6182
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-27 00:49:23 -0500

    Check for update toggle change before checking

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         else if is_key_pressed(KeyCode::F4) {
             primary_board = Board::glider_gun()
         }
-        if is_key_pressed(KeyCode::F2) || update_toggle {
-            primary_board.update();
-        }
         if is_key_pressed(KeyCode::F3) {
            update_toggle = !update_toggle;
+        }
+        if is_key_pressed(KeyCode::F2) || update_toggle {
+            primary_board.update();
         }
 
         primary_board.macroquad_draw();
